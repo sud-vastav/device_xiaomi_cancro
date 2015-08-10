719c75d6bb3113e685fa602e9056b29c30448985
sud-vastav <sudvastavs@gmail.com>
2015-08-10 13:41:30 +0530

    droid-ify
    droid-ify

--- crdroid.mk
+++ crdroid.mk
@@ -23,12 +23,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/crdroid/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+# Inherit some common crdroid stuff.
+$(call inherit-product, vendor/crdroid/config/common_mini_phone.mk)
 
-PRODUCT_NAME := cm_cancro
+PRODUCT_NAME := crdroid_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
